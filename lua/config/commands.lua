38f7f069bb92637630f3cd5a14da2f74dd7906c6
Christian Norrman <christian.norrman@consid.se>
2026-04-22 20:22:31 +0200

    wip: added pack clean / delete commands

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -7,3 +7,21 @@ end, { desc = "Update packages" })
 command("PackUpdateForce", function()
   vim.pack.update(nil, { force = true })
 end, { desc = "Update packages (force)" })
+
+command("PackDelete", function(opts)
+  vim.pack.del(opts.fargs)
+end, { desc = "Delete package", nargs = "+" })
+
+command("PackClean", function()
+  local inactivePacks = vim
+    .iter(vim.pack.get())
+    :filter(function(pkg)
+      return not pkg.active
+    end)
+    :map(function(pkg)
+      return pkg.spec.name
+    end)
+    :totable()
+
+  vim.pack.del(inactivePacks)
+end, { desc = "Cleanup inactive packages" })
